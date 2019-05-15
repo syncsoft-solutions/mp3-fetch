2338ef1f6bf52ebaebeb1f2dd935d6cc68d4a54e
alexiusacademia <alexius.academia@gmail.com>
2019-05-15 11:11:55 +0800

    Basic features created.

---
+++ mp3-fetch.py
@@ -0,0 +1,47 @@
+"""
+Downloads all mp3 file from a given web page.
+"""
+from bs4 import BeautifulSoup, SoupStrainer
+import requests
+from six.moves import urllib
+import os, shutil
+import sys
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 2:
+        print('Wrong number of inputs. Please specify the url then the folder to save the mp3 files.')
+    else:
+        url = args[1]
+        dir_path = os.getcwd()
+        sub_folder = os.path.join(args[2])
+
+        page = requests.get(url)
+
+        if os.path.isdir(sub_folder):
+            pass
+        else:
+            os.mkdir(sub_folder)
+
+        data = page.text
+
+        soup = BeautifulSoup(data, features="html.parser")
+
+        filenames = []
+        for link in soup.find_all('a'):
+            li = link.get('href')
+            ext = li[len(li) - 3:]
+
+            if ext == 'mp3':
+                download_link = url + li
+                # Download the file
+                fname = sub_folder + li
+                # print(fname)
+                filename, headers = urllib.request.urlretrieve(download_link)
+                # filenames.append((filename, li))
+                os.rename(filename, os.path.join(sub_folder, li))
+                print('Music downloaded successfully: ' + li)
